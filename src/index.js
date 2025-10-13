f4da017dda2da200a4da3a7e73a58b591ce4d02e
Satharva2004 <satharva2004@gmail.com>
2025-10-13 23:52:55 +0530

    added cros

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+  res.header('Access-Control-Allow-Origin', 'http://192.168.1.27:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.get('/', (req, res) => {
   res.json({ message: 'API is running' });
 });
